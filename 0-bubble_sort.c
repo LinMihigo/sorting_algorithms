23354dbacde8672b38522cc2e26151f2e743e3e2
LinMihigo <linmihigo@gmail.com>
2024-10-27 18:22:40 +0200

    bubble sort (1st fix)

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -19,25 +19,25 @@ void swap(int *x, int *y)
 */
 void bubble_sort(int *array, size_t size)
 {
-	size_t i, j, k;
+	size_t i, j;
+	bool swapped;
 
-	print_array(array, size);
-	if (sizeof(array) > 1)
+	if (!array || size < 2)
+		return;
+
+	for (i = 0; i < size - 1; i++)
 	{
-		for (i = 0; i < size - 1; i++)
+		swapped = false;
+		for (j = 0; j < size - i - 1; j++)
 		{
-			k = 1;
-			for (j = 0; j < size - i - 1; j++)
+			if (array[j] > array[j + 1])
 			{
-				if (array[j] > array[j + 1])
-				{
-					swap(&array[j], &array[j + 1]);
-					print_array(array, size);
-					k = 1;
-				}
+				swap(&array[j], &array[j + 1]);
+				print_array(array, size);
+				swapped = true;
 			}
-			if (k == 0)
-				break;
 		}
+		if (swapped == false)
+			break;
 	}
 }
